3235f45adb512f8a35d93edcd38a7a521efc7b80
Kyle Vance <91154892+KVance1010@users.noreply.github.com>
2023-04-26 16:08:54 -0700

    added sercices

---
+++ src/utils/services.js
@@ -0,0 +1,54 @@
+const serviceList =() => {
+    const services = [
+    {
+        service : 'Project Management',
+        description: 'Our team brings decades of combined experience to your projects. Together with the key stakeholders from your facility we will determine the project goals and deliverables so the team hired to design and construct your project will have clear and precise deliverables. These deliverables will be reviewed and tracked over the course of your project, and, together as a team, we will steer your project through design, permitting, construction, commissioning and project closeout to ensure your construction experience is a positive one.'
+    },
+    {
+            service : 'Scheduling',
+            description: 'Scheduling is more than just activities used for construction; it is a map to success. At the onset of your project, a schedule will be established showing all key deliverables and dates as well as key decisions that will need to be made in order to meet your deadlines and reach your project goals.'
+    },
+    {
+            service : 'Constructability Review',
+            description: 'This is the cornerstone of the cost control process. We include in-house reviews as well as consultant reviews, when appropriate, to detect any issues that have the potential to cost the client additional time and money.'
+    },
+    {
+            service : 'Value Engineering',
+            description: 'Providing common sense value engineering is a difficult task. We work with our clients to understand the key deliverables and where alterations can be made without affecting performance, quality and long term maintenance costs. In addition, we will provide cost savings measures to ensure your project stays on budget while maintaining the original intent of the project.'
+    },
+    {
+            service : 'Asset / Infrastructure Assessment',
+            description: 'We provide a thorough and cost-effective assessment of your existing building infrastructure and systems to determine the current condition, efficiency and remaining effective life of the equipment to determine the appropriate course of action. In conjunction with the facility, we develop a plan of maintenance or replacement that meets your needs and budget while continuing to provide the environment and services your employees and patients require.'
+    },
+    {
+            service : 'Cost Estimating',
+            description: 'Our team will use up-to-date historical pricing and analytics to ensure our clients have honest budgets that are attainable and accurate. While most firms utilize general contractors to provide budget services, we utilize our in-house staff to create and maintain independent, 3rd party project budgets.'
+    },
+    {
+            service : 'Sustainability',
+            description: 'From small tenant improvements to ground-up projects, our LEED Accredited Professionals will help certify your project while improving your building\'s energy consumption, water efficiency, CO2 emissions reduction, and indoor environmental quality. By applying our knowledge of the U.S. Green Building Council\'s certification process we will assist you in determining which credits will apply while meeting your facilities goals of sustainability.'
+    },
+    {
+            service : 'ADA Compliance (CASp)',
+            description: 'We specialize in assisting facilities in achieving compliance with the intricate Federal Codes of the Americans with Disabilities Act (ADA) and the complex California State Accessibility laws. Our team will assist you in determining where your facility is not in compliance with applicable codes and assist you in putting together a plan to mitigate your non-conforming issues.'
+    },
+    {
+            service : 'Quality Assurance (QA)',
+            description: 'We work closely with our clients to ensure that the product or service being developed is installed in compliance with the plans, specifications, and other project requirements at regular intervals and to minimize any defects before they go into the final product.'
+    },
+    {
+            service : 'Facility Management Consulting',
+            description: 'Our approach is centered on working in partnership with our clients to identify any deficiencies in their maintenance program which will help decrease repair costs, increase the life of their equipment, keep our clients within regulatory compliance (Joint Commission/CMS), reduce unnecessary repairs, and minimize downtime.'
+    },
+    {
+            service : 'Licensing & Regulatory Compliance',
+            description: 'We maintain strong relationships with OSHPD, CDHP, the Joint Commission and CMS to assist our clients in managing the code and regulatory aspects of healthcare compliance.'
+    },
+    {
+            service : 'Inspector of Record',
+            description: 'Our OSHPD Inspectors play a key role in the management, planning, and successful execution of projects which require working with decision-makers, project managers, architects, and owners on facility development. We will provide personal supervision of sub-consultants, special inspectors and/or material field testing technicians.'
+    }];
+    return services;
+}
+
+export default serviceList;
