8a90579e750343a2718c03a9e692c94ad251fa5f
Kyle Vance <91154892+KVance1010@users.noreply.github.com>
2023-04-25 18:43:26 -0700

    added a regex to get the first 20 words of discription for the carousel cards.

--- src/components/CarouselCard.js
+++ src/components/CarouselCard.js
@@ -5,10 +5,11 @@ import { useNavigate } from 'react-router-dom';
 
 function CarouselCard({ project, handleCurrentProject }) {
 	const navigate = useNavigate();
-	function handleProjectSave(){
-		handleCurrentProject(project)
-		navigate('/project',  {state: {currentProject: {project}}});
+	function handleProjectSave() {
+		handleCurrentProject(project);
+		navigate('/project', { state: { currentProject: { project } } });
 	}
+	const pattern =/(^(?:\S+\s+\n?){1,20})/;
 	return (
 		<div className="carousel_Project_card">
 			<hr className="head_line_break" />
@@ -17,15 +18,17 @@ function CarouselCard({ project, handleCurrentProject }) {
 				<p className="project_location">{project.location}</p>
 			</div>
 			<hr className="Lower_line_break "></hr>
-			<p className="carousel_padding">{project.description_short}</p>
+			<p className="carousel_padding">
+				{(project.description.match(pattern) + "...")}
+			</p>
 			<hr className="Lower_line_break"></hr>
-			<div className="project_link" onClick = {handleProjectSave} >
+			<div className="project_link" onClick={handleProjectSave}>
 				<div className="carousel_padding">
 					<img src={project.img} />
 				</div>
 				<div className="project_link arrow">
 					<span>VIEW PROJECT</span>
-					<img className='arrow_link' src={arrow} alt="right arrow" />
+					<img className="arrow_link" src={arrow} alt="right arrow" />
 				</div>
 			</div>
 			<hr className="Lower_line_break"></hr>
