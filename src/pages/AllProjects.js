7c04415dd913a804da448c0dd25693ef4455b8e5
Kyle Vance <91154892+KVance1010@users.noreply.github.com>
2023-04-26 21:13:45 -0700

    added animation to the projects and servies pages

--- src/pages/AllProjects.js
+++ src/pages/AllProjects.js
@@ -1,13 +1,13 @@
 import projectFile from '../utils/projectFile';
 import { useNavigate } from 'react-router-dom';
+import Slide from 'react-reveal/Slide';
 import '../css/pages/AllProjects.css';
 
 const AllProjects = ({ handleCurrentProject }) => {
-	
 	// returns an array of projects
 	let projects = projectFile();
 
-	// 
+	//
 	const navigate = useNavigate();
 
 	// Redirects to the current project page
@@ -15,21 +15,22 @@ const AllProjects = ({ handleCurrentProject }) => {
 		handleCurrentProject(project);
 		navigate('/project', { state: { currentProject: { project } } });
 	}
-	
+
 	return (
-		<div className='allProjectsContainer'>
+		<div className="allProjectsContainer">
 			<h2>PROJECTS</h2>
 			<hr />
 			<div className="allProjects">
 				{projects.map((project) => (
-					<div className="imageCard" key={project.id} onClick={() => handleProjectNavigate(project)}>
-						<img
-							className="projectImg"
-							src={project.img}
-							alt={project.alt}
-						/>
-						<p className="projectTitle">{project.businessName}</p>
-					</div>
+					<Slide bottom key={project.id}>
+						<div
+							className="imageCard"
+							onClick={() => handleProjectNavigate(project)}
+						>
+							<img className="projectImg" src={project.img} alt={project.alt} />
+							<p className="projectTitle">{project.businessName}</p>
+						</div>
+					</Slide>
 				))}
 			</div>
 		</div>
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import serviceList from '../utils/services.js';
+import Fade from 'react-reveal/Fade';
 import '../css/pages/Services.css';
 
 const Services = () => {
 	// Creates a services array from the services folder
 	const services = serviceList();
 	return (
-		<div className='servicesContainer'>
+		<div className="servicesContainer">
 			<h2>Services</h2>
 			<hr />
 			<div className="allServices">
 				{services.map((service) => (
-					<div
-						className="serviceCard"
-						key={service.id}
-					>
-						<h3 className="serviceTitle">{service.service}</h3>
-						<p className="serviceDescription">{service.description}</p>
-					</div>
+					<Fade bottom cascade key={service.id}>
+						<div className="serviceCard">
+							<h3 className="serviceTitle">{service.service}</h3>
+							<p className="serviceDescription">{service.description}</p>
+						</div>
+					</Fade>
 				))}
 			</div>
 		</div>
