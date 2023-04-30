b48fc21c6d41a0397008d84725f3f58d76928629
Kyle Vance <91154892+KVance1010@users.noreply.github.com>
2023-04-30 12:07:50 -0700

    working on css on home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -19,13 +19,12 @@ const HomePage = ({handleCurrentProject}) => {
 				width="44"
 				height="22"
 				viewBox="0 0 44 22"
-				fill="none"
+				fill="#ff4622"
 				className="downArrow"
 				xmlns="http://www.w3.org/2000/svg"
 			>
 				<path
 					d="M44.0009 3.86481L22.2785 21.9082L0.556091 3.86481L4.41182 0.66211L22.2785 15.5028L40.1452 0.66211L44.0009 3.86481Z"
-					fill="#ff4622"
 				/>
 			</svg>
 		</div>
