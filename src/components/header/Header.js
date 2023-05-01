fd7ddde67e6d7cc98e7b028eaa6ba1a8b300a9a7
Kyle Vance <91154892+KVance1010@users.noreply.github.com>
2023-04-30 17:00:45 -0700

    added animation to company logo

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,26 @@
-import React, {useState} from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import NavBar from './Navbar';
 import logo from '../../images/logo_porter_black.svg';
-import logoVideo from '../../images/porter-cropped.mp4';
-import '../../css/header/Header.css'
+import logoVideo from '../../images/Porter_Logo_Video.mp4';
+import '../../css/header/Header.css';
 
 function Header() {
 	const [dropDown, setDropdown] = useState(false);
 	const [activeState, setActiveState] = useState('');
+	const [endVideo, setEndVideo] = useState('logoVid');
+	const [showLogo, setShowLogo] = useState('displayNone');
+
+	const handleVideoEnd = () => {
+		if (endVideo === 'logoVid') {
+			setEndVideo('displayNone');
+			setShowLogo('logoImg');
+		} else {
+			setEndVideo('logoVid');
+			setShowLogo('displayNone');
+		}
+	};
+
 	const handleDropDown = () => {
 		setDropdown((dropDown) => !dropDown);
 		if (activeState === 'active') {
@@ -16,38 +29,42 @@ function Header() {
 			setActiveState('active');
 		}
 	};
-    return(
-        <header className="headerContainer">
+	return (
+		<header className="headerContainer">
 			<Link to="/" className="logoLink">
-				{/* <div className="logoVid">
-				<video src={logoVideo} autoPlay muted preload="auto"/>
-				</div> */}
-				<img
-					className="logoImg"
-					src={logo}
-					alt="Porter logo"
-				/>
+				<div className={`${endVideo}`}>
+					<video
+						src={logoVideo}
+						onEnded={handleVideoEnd}
+						autoPlay
+						muted
+						preload="auto"
+					/>
+				</div>
+				<img className={`${showLogo}`} src={logo} alt="Porter logo" />
 			</Link>
 			<nav className="navContainer">
 				<ul className="navbarNav">
-					<NavBar/>
+					<NavBar />
 				</ul>
 			</nav>
-			<div className={`hamburger navContainer ${activeState}`} onClick={handleDropDown}>
-					<span className="bar"></span>
-					<span className="bar"></span>
-					<span className="bar"></span>
-					{dropDown ? (
-						<ul className="navbarDropdown">
-							<NavBar	/>
-						</ul>
-					) : (
-						<span></span>
-					)}
-				</div>
+			<div
+				className={`hamburger navContainer ${activeState}`}
+				onClick={handleDropDown}
+			>
+				<span className="bar"></span>
+				<span className="bar"></span>
+				<span className="bar"></span>
+				{dropDown ? (
+					<ul className="navbarDropdown">
+						<NavBar />
+					</ul>
+				) : (
+					<span></span>
+				)}
+			</div>
 		</header>
-    )
-
+	);
 }
 
 export default Header;
